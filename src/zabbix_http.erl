f5cad2c9d5cc9bb8af81cbf71f67a3f47588f811
Mikhail Kuznetsov <mike@botforce.com>
2014-11-06 15:01:34 +0300

    add counter aggregation, set interval

--- src/zabbix_http.erl
+++ src/zabbix_http.erl
@@ -12,7 +12,9 @@
 -behaviour(gen_fsm).
 -include("zabbix_http.hrl").
 %% API
--export([start_link/0, start_link/2, log/2,log/4, log/1, set_host_port/2, ts/0, hostname/0, flush/0]).
+-export([start_link/0, start_link/2,
+         log/2,log/4, log/1, flush/0,
+         set_host_port/2, set_interval/1, agregate/1, ts/0, hostname/0 ]).
 
 %% gen_fsm callbacks
 -export([init/1,
@@ -29,7 +31,7 @@
 -define(SERVER, ?MODULE).
 -define(HTTP_REQ_TIMEOUT,60000).
 -define(COLLECTION_INTERVAL,30000).
--record(state, {cache, host,port}).
+-record(state, {cache, host,port, interval=?COLLECTION_INTERVAL, agregate=[]}).
 %%--------------------------------------------------------------------
 %% @doc
 %% Set host and port to conect to Zabbix
@@ -39,6 +41,25 @@
 set_host_port(Host,Port)->
   gen_fsm:sync_send_event(?SERVER,{set_host_port,Host,Port}).
 
+%%--------------------------------------------------------------------
+%% @doc
+%% Set data buffer flush interval (seconds) for Zabbix metrics data. We buffer data, to reduce load on Zabbix and provide agregation for data.
+%% Default interval is 30 sec.
+%% @end
+%%--------------------------------------------------------------------
+set_interval(Interval) when is_number(Interval), Interval>0 ->
+  gen_fsm:sync_send_event(?SERVER,{set_interval,Interval*1000}).
+
+%%--------------------------------------------------------------------
+%% @doc
+%% Set list of metrics to predagregate for Zabbix. It is very usefull for counter related data (like queries per minute or money transactions). We can aggregate only numeric values. Agregation is a sum of listed metrics per node per interval.
+%% By default data is not agregated.
+%% To disable it call with [] as argument.
+%% To enable it put list of metrics as argument, like this [<<"new_users">>, <<"cc_transactions_total">>]
+%% @end
+%%--------------------------------------------------------------------
+agregate(MetricsList) when is_list(MetricsList) ->
+  gen_fsm:sync_send_event(?SERVER,{agregate,MetricsList}).
 %%--------------------------------------------------------------------
 %% @doc
 %% Force to flush inner buffer and send packet to zabbix
@@ -100,6 +121,10 @@ start_link() ->
 start_link(Host,Port) ->
   gen_fsm:start_link({local, ?SERVER}, ?MODULE, [Host,Port], []).
 
+
+stop() ->
+  gen_fsm:sync_send_event(?SERVER, stop).
+
 %%%===================================================================
 %%% gen_fsm callbacks
 %%%===================================================================
@@ -133,7 +158,7 @@ init([H,P]) ->
    timeout() | hibernate} |
   {stop, Reason :: term(), NewState :: #state{}}).
 idle(Event, State) ->
-  gen_fsm:send_event_after(?COLLECTION_INTERVAL, post_events),
+  gen_fsm:send_event_after(State#state.interval, post_events),
   collecting(Event,State).
 %%--------------------------------------------------------------------
 %% @private
@@ -148,19 +173,31 @@ idle(Event, State) ->
   {stop, Reason :: term(), NewState :: #state{}}).
 collecting(#event{}=Event, State) ->
   collecting([Event], State);
-collecting([#event{}|_]=Events, State) ->
+collecting([#event{}|_]=Events, State=#state{agregate = []}) ->
   ets:insert(State#state.cache, [{make_ref(), Event}||Event<-Events]),
   {next_state, collecting, State};
+
+collecting([#event{}|_]=Events, State=#state{agregate = MetricsList, cache = Tab}) ->
+  [case lists:member(M, MetricsList) of
+     true->
+       ets:insert_new(Tab, {{M,H},Event, V}) orelse ets:update_counter(Tab,{M,H}, [{3, V}]);
+     false->
+       ets:insert(Tab, {make_ref(), Event})
+   end||#event{metric=M,host=H, value=V}=Event<-Events],
+  {next_state, collecting, State};
 collecting(post_events, State=#state{cache = Tab, host=H,port = P}) ->
-  % TODO: limit send list in case of it become too big
-  EventList=[E||{_,E}<-ets:tab2list(Tab)],
+  Ts=ts(),
+  EventList=[case E of
+               {_,Event}->Event;
+               {_,Event,V}->Event#event{value=V, timestamp=Ts}
+             end||E<-ets:tab2list(Tab)],
   case send(H,P, to_json(EventList)) of
     ok->
       ets:delete_all_objects(Tab),
       {next_state, idle, State};
     {error, _Rsp }->
       % if we have an error or some other fail
-      gen_fsm:send_event_after(?COLLECTION_INTERVAL, post_events),
+      gen_fsm:send_event_after(State#state.interval, post_events),
       {next_state, collecting, State}
   end.
 
@@ -183,12 +220,24 @@ collecting(post_events, State=#state{cache = Tab, host=H,port = P}) ->
             {stop, Reason :: normal | term(), Reply :: term(),
              NewState :: #state{}}).
 idle({set_host_port,H,P}, _From, State) ->
-  Reply = ok,
-  {reply, Reply, idle, State#state{host=H,port = P}};
+  {_,Reply,_, NewState}=collecting({set_host_port,H,P}, _From, State),
+  {reply, Reply, idle, NewState};
+
+idle({set_interval, Interval}, _From, State)->
+  {_,Reply,_, NewState}=collecting({set_interval, Interval}, _From, State),
+  {reply, Reply, idle, NewState};
+
+idle({agregate, MetricsList}, _From, State) ->
+  {_,Reply,_, NewState}=collecting({agregate, MetricsList}, _From, State),
+  {reply, Reply, idle, NewState};
 
 idle(flush, _From, State) ->
   Reply = ok,
-  {reply, Reply, idle, State}.
+  {reply, Reply, idle, State};
+
+idle(stop, _From, State) ->
+  Reply = ok,
+  {stop, normal, Reply, State}.
 %%--------------------------------------------------------------------
 %% @private
 %% @doc
@@ -209,10 +258,23 @@ idle(flush, _From, State) ->
 collecting({set_host_port, H,P}, _From, State) ->
   Reply = ok,
   {reply, Reply, collecting, State#state{host=H,port=P}};
+
+collecting({set_interval, Interval}, _From, State) ->
+  Reply = ok,
+  {reply, Reply, collecting, State#state{interval = Interval}};
+
+collecting({agregate, MetricsList}, _From, State) ->
+  Reply = ok,
+  {reply, Reply, collecting, State#state{agregate = MetricsList}};
+
 collecting(flush, _From, State) ->
   Reply = ok,
   gen_fsm:send_event_after(10, post_events),
-  {reply, Reply, collecting, State}.
+  {reply, Reply, collecting, State};
+
+collecting(stop, _From, State) ->
+Reply = ok,
+{stop, normal, Reply, State}.
 
 -spec(handle_event(Event :: term(), StateName :: atom(),
                    StateData :: #state{}) ->
@@ -291,7 +353,6 @@ send(Host, Port, Data)->
                        [{active,false},
                         {packet,0}]) of
     {ok, Socket} ->
-      io:format("Socket =~p~n",[Socket]),
       ok=gen_tcp:send(Socket, Data),
       ok=gen_tcp:close(Socket);
     Other ->
@@ -315,6 +376,9 @@ workflow_test_() ->
      }
      ,{<<"Inner flow">>,
       fun try_inner_workflow/0
+     }
+     ,{<<"Counter agregation">>,
+       fun try_agragate/0
      }
    ]}.
 
@@ -342,6 +406,26 @@ try_fsm_workflow()->
   timer:sleep(200),
   ?debugFmt("Status val=~p status=~p",[Val,Status]),
   ?assertEqual(ok,Status),
+  stop(),
   ok.
 
+try_agragate()->
+  H= <<"fmdev-e1.dev.fly.me">>,
+  random:seed(now()),
+  Val1=random:uniform(20),
+  Val2=random:uniform(20),
+  Val3=random:uniform(20),
+
+  start_link(),
+  set_host_port("zabbix.dev.fly.me",10051),
+  agregate([<<"3ds_authorize_declinded_by_antifrod-filter">>]),
+  Status = [log([{<<"3ds_authorize_declinded_by_antifrod-filter">>,Val1,H, ts()}]),
+            log([{<<"3ds_authorize_declinded_by_antifrod-filter">>,Val2,H, ts()}]),
+            log([{<<"3ds_authorize_declinded_by_antifrod-filter">>,Val3,H, ts()}])],
+  flush(),
+  timer:sleep(200),
+  ?debugFmt("Status val=~p status=~p",[Val1+Val2+Val3,Status]),
+  ?assertEqual([ok,ok,ok],Status),
+  stop(),
+  ok.
 -endif.
